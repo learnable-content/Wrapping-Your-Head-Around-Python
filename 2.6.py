ba86612c76b193a13792aae15c31485ebb410dab
bitesizebschool <brettromero@MacBook-Pro.local>
2016-03-14 09:04:59 -0700

    Fix spelling error

--- 2.6.py
+++ 2.6.py
@@ -1,6 +1,6 @@
 fileOutput = open('/users/brettromero/test/file_io.txt', 'wt')
 fileOutput.write('John Doe;\n350 5th Ave\n|\nJane Doe;\n100 7th Ave\n|\nJoe Daniels;\n11 1st Ave\n')
-fileOuptput.close()
+fileOutput.close()
 
 fileInput = open('/users/brettromero/test/file_io.txt', 'rt')
 fileInput.read()
