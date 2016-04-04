563ede3315592cce6c76e6311154fbec7b0744cf
bitesizebschool <brettromero@MacBook-Pro.local>
2016-04-03 20:05:28 -0700

    Removing output from file.

--- 2.1.py
+++ 2.1.py
@@ -1,30 +1,22 @@
->>> customer = "John Doe"
->>> pickupLocation = "350 5th Ave"
->>> package = customer + pickupLocation
->>> print(package)
-John Doe350 5th Ave
->>> 
+customer = "John Doe"
+pickupLocation = "350 5th Ave"
+package = customer + pickupLocation
+print(package)
+
 package = customer + "@" + pickupLocation
->>> print(package)
-John Doe@350 5th Ave
->>> fare = 15
->>> tip = 2
->>> totalFare = fare + tip
->>> print(totalFare)
-17
->>> print(package + " with total of " + totalFare)
-Traceback (most recent call last):
-  File "<pyshell#21>", line 1, in <module>
-    print(package + " with total of " + totalFare)
-TypeError: Can't convert 'int' object to str implicitly
->>> 
->>> print(package + " with total of " + str(totalFare))
-John Doe@350 5th Ave with total of 17
->>> fare + "2"
-Traceback (most recent call last):
-  File "<pyshell#24>", line 1, in <module>
-    fare + "2"
-TypeError: unsupported operand type(s) for +: 'int' and 'str'
->>> fare + int("2")
-17
->>> 
+print(package)
+
+fare = 15
+tip = 2
+totalFare = fare + tip
+print(totalFare)
+
+#creates error
+#print(package + " with total of " + totalFare)
+
+print(package + " with total of " + str(totalFare))
+
+#creates error
+#fare + "2"
+
+fare + int("2")
